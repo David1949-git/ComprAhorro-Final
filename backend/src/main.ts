4847e1a5aa22ce044753ebf4ed7c9ec4793c5379
david1949-git <tu_correo@ejemplo.com>
2026-04-04 19:51:22 -0500

    fix: usar process.env.PORT para compatibilidad con Render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const port = 10000;
+  const port = process.env.PORT || 10000;
   await app.listen(port);
   console.log(`🚀 Motor de ComprAhorro encendido en puerto: ${port}`);
 }
